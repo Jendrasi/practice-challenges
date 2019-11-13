5809934c7068c80e162c13ebb3da6d2d0f3d459c
Jendrasi <14114945+Jendrasi@users.noreply.github.com>
2019-11-13 15:46:08 -0500

    minChange - Add bottom up solution

--- minimum-change/change.py
+++ minimum-change/change.py
@@ -5,10 +5,28 @@ def __topDown(amt, denom):
     return 0
 
 def __bottomUp(amt, denom):
-    return 0
+    # This base combination for each table index represents a worst possible solution
+    absMaxCoins = [1]*(amt + 1)
+    table = [[]] + [absMaxCoins]*amt
+    
+    # Each index in this one dimensional table represents a combination of coins that adds up to the value of its index. The goal is to get the minimum coins for every one, building to the total amount. The last member of the table represents the total amount.
+    for i in range(len(table)):
+        # Skip zero-th entry (base case)
+        if i > 0:
+            # Try every possible coin
+            for coin in denom:
+                # Ignore coin if it is too big to fit into the sub amount
+                if coin <= i:
+                    subComb = table[i-coin]
+                    if (len(subComb)+1) < len(table[i]):
+                        table[i] = subComb+[coin]
+    return table[-1]
 
 def minimumChange(amt, denom, strat):
-    return 0
+    if strat:
+        return __topDown(amt, denom)
+    else:
+        return __bottomUp(amt, denom)
 
 def printSolution(solution):
     return 0
@@ -32,7 +50,8 @@ if __name__ == "__main__":
     except:
         sys.exit("Couldn't parse denominations.")
     if isinstance(d, list):
-        d = set(d)
+        d = list(dict.fromkeys(d))
+        d.sort()
     else:
         sys.exit("Denominations are not recognized as a list.")
     # ...get algorithm strategy
@@ -51,9 +70,11 @@ if __name__ == "__main__":
     print("Amount is",a)
     print("Denominations are:")
     print(*d,sep=', ')
-
+    
     # Execute
-    # solution = minimumChange(a,d,strategy)
+    # solution = minimumChange(6,[4,3,1],0)
+    solution = minimumChange(a,d,strategy)
 
     # Final Solution
+    print(solution)
     # printSolution(solution)
