6eab80b0d7424da62e05d4a2ec8d639fa40e1fcc
Jendrasi <14114945+Jendrasi@users.noreply.github.com>
2019-11-16 23:46:31 -0500

    minChange - topDown solution
    top down recursive added, plus fully commented

--- minimum-change/change.py
+++ minimum-change/change.py
@@ -1,67 +1,137 @@
-import sys
-import ast
-import time
-
+'''
+This function encapsulates the top down recursive method of solving this problem. It first recursively populates a decision tree with __appendChildren(p,d), then finds the shortest path from the top node to a leaf in a separate recursive traversal. The shortest path algorithm is abstracted away in the custom Node class.
+'''
 def __topDown(amt, denom):
-    return 0
 
+    from node import Node
+
+    '''
+    Recursive function to build out a decision tree from a top node and series of denominations. The top node/parent has the total amount we intend to optimally make with the denominations.
+    # NOTE: Since the parent is actually passed as a memory address, it can be accessed and modified directly, like a pointer. No returning the final tree into a new object.
+    # !: Warning that this part of the top down recursive solution is exponentially memory hungry with higher starting amounts and lists of denominations. Hoping to improve this.
+    '''
+    def __appendChildren(parent, denom):
+        for d in denom:
+            if d <= parent.amount:
+                childAmt = parent.amount - d
+                child = Node(childAmt)
+                if childAmt > 0:
+                    __appendChildren(child, denom)
+                parent.addChild(child)
+            else:
+                break
+
+    '''
+    Node.getMinDepth() returns the shortest path to a leaf, as a list of descending amounts as coins are subtracted. To get which coins were used, just subtract n+1 from n across the list.
+    # Ex: [6,2,1,0] -> [4,1,1]
+    # NOTE: The path p is expected to already be sorted in descending order the way it came from getMinDepth()
+    '''
+    def __pathToCoins(p):
+        # path.sort(reverse=true)
+        coins = []
+        for i in range(len(p)-1):
+            coins.append(p[i]-p[i+1])
+        return coins
+
+    # Initialize tree as a top node
+    tree = Node(amt)
+    # Populate it's children and their children recursively
+    # !: This part could use optimization.
+    __appendChildren(tree, denom)
+    # Recursively traverse tree from top Node to find shortest path to a leaf
+    path = tree.getMinDepth()
+    # Convert the descending amounts in the shortest path to a list of coins
+    return __pathToCoins(path)
+
+
+'''
+This function encapsulates the bottom up iterative method of solving this problem. It does all the work in a one dimensional list with a length of amount + 1. For each index i, the value becomes a list of the best coins to make i. This builds on the knowledge of how to make base cases 0 and 1, like a mathematical proof. It is well optimized and does not redo any work.
+'''
 def __bottomUp(amt, denom):
-    # This base combination for each table index represents a worst possible solution
+    # Create an example starting 'solution' that will always be beaten... A list of 1 dollar coins.
     absMaxCoins = [1]*(amt + 1)
+    # The table at index 0 is an empty list. This represents an accepted base case, that you can make an amount of i=0 with 0 coins.
     table = [[]] + [absMaxCoins]*amt
     
-    # Each index in this one dimensional table represents a combination of coins that adds up to the value of its index. The goal is to get the minimum coins for every one, building to the total amount. The last member of the table represents the total amount.
+    # Iterate the table.
     for i in range(len(table)):
-        # Skip zero-th entry (base case)
+        # Skip zero-th entry (base case).
         if i > 0:
-            # Try every possible coin
+            # Try to subtract every denomination that fits into i.
             for coin in denom:
-                # Ignore coin if it is too big to fit into the sub amount
                 if coin <= i:
+                    # Take the known best combination at table[i-coin], and add the coin to it to make a possible 'best' combination to make i.
                     subComb = table[i-coin]
+                    # If the new list of coins is shorter than the previous solution to make i, replace it at table[i].
                     if (len(subComb)+1) < len(table[i]):
                         table[i] = subComb+[coin]
+    # By the time the table is iterated, the final index will contain the minimal combination of coins to make the amount.
     return table[-1]
 
+
+'''
+Publicly visible function to execute the problem, given an amount, denomination list, and strategy (1 for 'topDownRecursive' or 0 for 'bottomUpIterative').
+'''
 def minimumChange(amt, denom, strat):
+    # __topDown().__appendChildren() assumes that the denomations will be sorted in ascending order to save a few unnecessary if checks. So denominations list is presorted.
+    denom.sort()
+    # Execute the requested strategy and return the result as a minimal list of coins.
     if strat:
         return __topDown(amt, denom)
     else:
         return __bottomUp(amt, denom)
 
-def printSolution(solution):
-    return 0
 
+'''
+Publicly visible function to print a formatted solution.
+'''
+def printSolution(solution):
+    output = 'The minimal change is '
+    amt = 0
+    for coin in solution:
+        output += str(coin) + ' + '
+        amt += coin
+    print(output[:-2] + '= ' + str(amt))
+
+
+'''
+Main execution
+# NOTE: Commented out execution time because this method of measuring performance is not very scientific. Not accurate or precise, especially on Windows.
+'''
 if __name__ == "__main__":
+    from ast import literal_eval
+    from sys import argv, exit
+    # import time
+
     # Input validation
-    args = sys.argv
+    # args = argv
     argErr = "Bad arguments\nUsage: python change.py amountInteger denomList [topDown|bottomUp]"
     # ...check min number of args
-    if len(args) < 3:
-        sys.exit(argErr)
+    if len(argv) < 3:
+        exit(argErr)
     # ...get amount
-    a = args[1]
+    a = argv[1]
     if not a.isnumeric():
-        sys.exit("Amount is not an integer.")
+        exit("Amount is not an integer.")
     a = int(a)
     # ...get denominations
-    d = args[2]
+    d = argv[2]
     try:
-        d = ast.literal_eval(d)
+        d = literal_eval(d)
     except:
-        sys.exit("Couldn't parse denominations.")
+        exit("Couldn't parse denominations.")
     if isinstance(d, list):
         d = list(dict.fromkeys(d))
         d.sort()
     else:
-        sys.exit("Denominations are not recognized as a list.")
+        exit("Denominations are not recognized as a list.")
     # ...get algorithm strategy
     strategy = 0
-    if len(args) > 3:
-        if args[3] == "topDown":
+    if len(argv) > 3:
+        if argv[3] == "topDown":
             strategy = 1
-        elif args[3] != "bottomUp":
-            sys.exit("Optional algorithm strategy not recognized.")
+        elif argv[3] != "bottomUp":
+            exit("Optional algorithm strategy not recognized.")
 
     # Pre-run user validation of input
     if strategy:
@@ -76,15 +146,15 @@ if __name__ == "__main__":
     # startT = time.time_ns()
 
     # Execute
-    # solution = minimumChange(6,[4,3,1],0)
+    # solution = minimumChange(24,[5,2,1],1)
     solution = minimumChange(a,d,strategy)
 
     # Execution Time (cont.)
     # endT = time.time_ns()
 
     # Final Solution
-    print(solution)
-    # printSolution(solution)
+    # print(solution)
+    printSolution(solution)
 
     # Execution Time (fin.)
     # print("Algorithm Execution Time: %s milliseconds" % ((endT-startT)/1000000))
--- minimum-change/node.py
+++ minimum-change/node.py
@@ -1,6 +1,6 @@
 class Node:
     # Constructor, requires amount value.
-    # NOTE self.parent is commented out for this implementation because a parent address does not need to be accessed from it's child.
+    # NOTE: self.parent is commented out for this implementation because a parent address does not need to be accessed from it's child.
     def __init__(self, amt):
         self.amount = amt
         self.children = []
@@ -16,11 +16,14 @@ class Node:
         return len(self.children) > 0
 
     # Recursive function to get a list of child nodes that represent the shortest leaf from 'self/this' Node.
+    # NOTE: In an attempt to improve performance and memory usage, I commented out the option to return a list of Nodes, and instead only return a list of the amounts of those Nodes, because that's all I need to know which coins were used.
     def getMinDepth(self):
         if not self.hasChildren():
             return [self.amount]
+            # return [self]
         depths = []
         for child in self.children:
             d = child.getMinDepth()
             depths.append(d)
         return [self.amount] + min(depths,key=len)
+        # return [self] + min(depths,key=len)
