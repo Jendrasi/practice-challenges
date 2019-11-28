1be5647c3d154735ec43536a45d67671156bde5f
Jendrasi <14114945+Jendrasi@users.noreply.github.com>
2019-11-28 00:37:56 -0500

    Start shuffle practice
    Created package and class for simple shuffle practice

---
+++ Java/src/shuffle/Main.java
@@ -0,0 +1,22 @@
+package shuffle;
+
+public class Main {
+
+	public static int[] shuffleFisherYates(int[] list) {
+		int[] result = list;
+		
+		return result;
+	}
+	
+	public static int[] shuffleDurstenfeld(int[] list) {
+		int[] result = list;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
