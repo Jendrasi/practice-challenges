88af8acd461566e1f60ae2393a2b863611116c0b
Jendrasi <14114945+Jendrasi@users.noreply.github.com>
2024-02-06 16:05:14 -0500

    merge names problem

--- Java/testdome/src/mergenames/MergeNames.java
+++ Java/testdome/src/mergenames/MergeNames.java
@@ -1,9 +1,23 @@
 package mergenames;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MergeNames {
     
     public static String[] uniqueNames(String[] names1, String[] names2) {
-        throw new UnsupportedOperationException("Waiting to be implemented.");
+        Set<String> uniqueNames = new HashSet<String>();
+        int i = 0;
+        while (i < names1.length) {
+            uniqueNames.add(names1[i]);
+            i++;
+        }
+        i = 0;
+        while (i < names2.length) {
+            uniqueNames.add(names2[i]);
+            i++;
+        }
+        return uniqueNames.stream().toArray(String[]::new);
     }
     
     public static void main(String[] args) {
@@ -11,4 +25,5 @@ public class MergeNames {
         String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
         System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
     }
+
 }
